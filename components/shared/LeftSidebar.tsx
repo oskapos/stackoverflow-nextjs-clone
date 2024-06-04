a398ccbc7feb74dbc6babe1bb00e113e0ebd3ce5
oskapos <omarblitz9@gmail.com>
2024-06-04 15:09:05 +0300

    leftSidebar & rightSidebar ui

---
+++ components/shared/LeftSidebar.tsx
@@ -0,0 +1,53 @@
+'use client';
+import React from 'react';
+import { sidebarLinks } from '@/constants';
+import Link from 'next/link';
+import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+import { SignedIn, SignedOut, useAuth } from '@clerk/nextjs';
+import { Button } from '@/components/ui/button';
+import { SheetClose } from '../ui/sheet';
+
+const LeftSidebar = () => {
+  const pathname = usePathname();
+  const { signOut } = useAuth();
+
+  return (
+    <section className='background-light900_dark200 light-border flex-between sticky left-0 top-0 flex h-screen flex-col gap-7 overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]'>
+      <div className='flex flex-1 flex-col gap-6'>
+        {sidebarLinks.map((item) => {
+          const isActive = (pathname.includes(item.route) && item.route.length > 1) || pathname === item.route;
+
+          return (
+            <Link
+              key={item.route}
+              href={item.route}
+              className={`${isActive ? `primary-gradient rounded-lg text-light-900` : `text-dark300_light900`} flex items-center justify-start gap-4 bg-transparent p-4`}
+            >
+              <Image src={item.imgURL} alt={item.label} width={20} height={20} className={`${isActive ? '' : 'invert-colors'}`} />
+              <p className={`${isActive ? 'base-bold' : 'base-medium'} max-lg:hidden`}>{item.label}</p>
+            </Link>
+          );
+        })}
+      </div>
+      <SignedOut>
+        <div className='flex w-full flex-col gap-3'>
+          <Link href='/sign-in'>
+            <Button className='small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none'>
+              <Image src='/assets/icons/account.svg' alt='login' width={20} height={20} className='invert-colors lg:hidden' />
+              <span className='primary-text-gradient max-lg:hidden'>Log in</span>
+            </Button>
+          </Link>
+          <Link href='/sign-up'>
+            <Button className='small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none'>
+              <Image src='/assets/icons/sign-up.svg' alt='login' width={20} height={20} className='invert-colors lg:hidden' />
+              <span className='max-lg:hidden'>Sign up</span>
+            </Button>
+          </Link>
+        </div>
+      </SignedOut>
+    </section>
+  );
+};
+
+export default LeftSidebar;
---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import RenderTag from './RenderTag';
+
+const questions = [
+  { _id: 1, title: 'How can an airconditioning machine exist?' },
+  { _id: 2, title: 'Would it be appropriate to point out an error in another paper during a referee report?' },
+  { _id: 3, title: 'Interrogated every time crossing UK Border as citizen' },
+  { _id: 4, title: 'Low digit addition generator' },
+  { _id: 5, title: 'What is an example of 3 numbers that do not make up a vector?' },
+];
+
+const popTags = [
+  { _id: 1, name: 'JS', totalQs: 5 },
+  { _id: 2, name: 'React', totalQs: 5 },
+  { _id: 3, name: 'Next', totalQs: 5 },
+  { _id: 4, name: 'Node', totalQs: 5 },
+  { _id: 5, name: 'Rust', totalQs: 5 },
+];
+
+const RightSidebar = () => {
+  return (
+    <section className='background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-7 overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden'>
+      <div>
+        <h3 className='h3-bold text-dark200_light900'>Top questions</h3>
+        <div className='mt-7 flex w-full flex-col gap-[30px]'>
+          {questions.map((q) => (
+            <Link key={q._id} href={`/questions/${q._id}`} className='flex cursor-pointer items-center justify-between gap-7'>
+              <p className='body-medium text-dark500_light700'>{q.title}</p>
+              <Image src='/assets/icons/chevron-right.svg' alt='chevron right' width={20} height={20} className='invert-colors' />
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className='mt-16'>
+        <h3 className='h3-bold text-dark200_light900'>Popular tags</h3>
+        <div className='mt-7 flex flex-col gap-4'>
+          {popTags.map((tag) => (
+            <RenderTag key={tag._id} _id={tag._id} name={tag.name} totalQs={tag.totalQs} showCount />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default RightSidebar;
