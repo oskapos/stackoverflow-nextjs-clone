6ecec3d8253b0550444927a87f3bc7f107755d6a
oskapos <omarblitz9@gmail.com>
2024-07-11 14:53:49 +0300

    create a question action

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -12,12 +12,19 @@ import { Badge } from '../ui/badge';
 import Image from 'next/image';
 import { useRef, useState } from 'react';
 import { createQuestion } from '@/lib/actions/question.action';
+import { usePathname, useRouter } from 'next/navigation';
 
 const type = 'edit';
 
-const Question = () => {
+interface Props {
+  mongoUserId: string;
+}
+
+const Question = ({ mongoUserId }: Props) => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const editorRef = useRef(null);
+  const router = useRouter();
+  const pathname = usePathname();
 
   const form = useForm<z.infer<typeof QuestionsSchema>>({
     resolver: zodResolver(QuestionsSchema),
@@ -31,7 +38,14 @@ const Question = () => {
   async function onSubmit(values: z.infer<typeof QuestionsSchema>) {
     setIsSubmitting(true);
     try {
-      await createQuestion({});
+      await createQuestion({
+        title: values.title,
+        content: values.explanation,
+        tags: values.tags,
+        author: JSON.parse(mongoUserId),
+      });
+
+      router.push('/');
     } catch (error) {
     } finally {
       setIsSubmitting(false);
@@ -154,7 +168,7 @@ const Question = () => {
                     )}
                   </>
                 </FormControl>
-                <FormDescription className='body-regular mt-2.5 text-light-500'>Add up to 3 tags to describe what the question is about. Your need to press enter to add a tag.</FormDescription>
+                <FormDescription className='body-regular mt-2.5 text-light-500'>Add up to 3 tags to describe what the question is about. You need to press enter to add a tag.</FormDescription>
                 <FormMessage className='text-red-500' />
               </FormItem>
             )}
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,11 +1,40 @@
 'use server';
 
-import { error } from 'console';
 import { connectToDatabase } from '../mongoose';
+import Question from '@/database/question.model';
+import Tag from '@/database/tag.model';
 
 export async function createQuestion(params: any) {
   try {
     await connectToDatabase();
+
+    const { title, content, tags, author, path } = params;
+
+    //create the question
+    const question = await Question.create({
+      title,
+      content,
+      author,
+    });
+
+    const tagsDocument = [];
+
+    //establish relations Question <--> Tag
+
+    //create the tags or get them if they already exist
+    for (const tag of tags) {
+      const existingTag = await Tag.findOneAndUpdate(
+        { name: { $regex: new RegExp(`^${tag}$`, 'i') } },
+        { $setOnInsert: { name: tag }, $push: { question: question._id } },
+        { upsert: true, new: true }
+      );
+
+      tagsDocument.push(existingTag._id);
+
+      await Question.findByIdAndUpdate(question._id, {
+        $push: { tags: { $each: tagsDocument } },
+      });
+    }
   } catch (error) {
     console.error(error);
   }
